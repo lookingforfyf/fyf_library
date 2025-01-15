6ed8a3b5482d754b0fe3c15cc8de6f0d8c884064
范云飞 <fyf786452470@gmail.com>
2025-01-15 15:43:43 +0800

    add private function

--- lib/src/calculator.dart
+++ lib/src/calculator.dart
@@ -5,6 +5,9 @@ class Calculator {
 
   int multiply(int a, int b) => a * b;
 
+  // 测试私有方法
+  int _privateAdd(int a, int b) => a + b;
+
   double divide(int a, int b) {
     if (b == 0) throw ArgumentError('Cannot divide by zero');
     return a / b;
